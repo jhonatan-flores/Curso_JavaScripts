ee8132f7b810946769abbeac9d24fe0563243685
jhonatan-flores <hennryflorescupe@gmail.com>
2024-10-18 12:14:24 -0500

    Feed:Clases de estroctura de un programa
    bindings entorno y controles de flujo

---
+++ 3.Estructura_del_Programa/entorno.js
@@ -0,0 +1,11 @@
+let cadena="hola"
+let saludo="como estas"
+let suma=45+42
+
+let edadPersona=18
+let respuesta=edadPersona>18 ? "ya estas viejo":"eres pulpin"
+
+//funciones de conversion Number
+// let elNumero=Number(prompt("Elije un numero"))
+let elNumero=prompt("Elije un numero")
+console.log(`tu numero es la raiz cuadrada de: ${(+elNumero)*(+elNumero)}`)
