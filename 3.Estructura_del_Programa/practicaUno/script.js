ebec04e9aebf81d23c249f6092952febd59036e2
jhonatan-flores <hennryflorescupe@gmail.com>
2024-10-18 12:23:42 -0500

    Feet:Tarea

---
+++ 3.Estructura_del_Programa/practicaUno/script.js
@@ -0,0 +1,9 @@
+
+// ejercicio uno
+let num1 = parseInt(prompt("Ingresa el primer número:"));
+let num2 = parseInt(prompt("Ingresa el segundo número:"));
+if (num1 * num2 > 100) console.log("El producto es mayor a 100");
+//ejercicio dos
+let password = prompt("Ingresa la contraseña:");
+if (password === "soy el mas perron aqui") console.log("Contraseña correcta");
+//ejercicio tres
