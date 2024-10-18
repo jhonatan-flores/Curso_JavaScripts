c31826f5ef102e8d34cd7ac39d8d621f4f497e1a
jhonatan-flores <hennryflorescupe@gmail.com>
2024-10-18 12:04:59 -0500

    Feet: clase ternario

---
+++ 2.Valores_Operadores/opTernario.js
@@ -0,0 +1,10 @@
+respuesta=5>7?"esta bien mascota":"como va a ser mayor animal"
+console.log(respuesta)
+
+//Ejercicio
+//usando el operador ternario evaluar si un numero es par o impar
+num=5
+console.log(num % 2 === 0 ? "Es par" : "Es impar") 
+//mostrar un mensaje si la edad de una persona es mayor a 18
+edad=19
+console.log(edad>18 ? "Eres mayor" : "eres menor")
