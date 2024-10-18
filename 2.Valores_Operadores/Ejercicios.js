525ff58eea9cc741d05a99a7bceab19627ce2589
jhonatan-flores <hennryflorescupe@gmail.com>
2024-10-18 12:02:40 -0500

    Feex:actializacion de de aritmetica

---
+++ 2.Valores_Operadores/Ejercicios.js
@@ -0,0 +1,24 @@
+//convertir expresiones aritmericas
+//expresion algoritmica
+//expresion lineal
+console.log(((10+3)/(6))+(4))
+console.log(5+(10/(3+2)))
+console.log(5+(10/2)+7)
+
+console.warn(((5/5)+(10/5))/((20/5)-(10/2)))
+console.warn(5+((10+((5+5)/(10+2)))/(20+(20/5))))
+console.warn(5+5+(5/2)+((10/(10-5))/(20/(10+5))))
+console.warn((10/(10+10))/(20/(10-5)))
+console.warn((5+(2/(5+2+(5/2)))/(10+(10/(5+5)))))
+console.warn((5+5+5)/(5+(5/2)))
+console.warn((5+5+(9/(5*5)))/((2+2)*(10/5)))
+console.warn((10+(5/2)+2)/2)
+
+//crear una variable de numero y mostrar por terminal el numero sumado mas uno
+num=2
+console.log(++num)
+
+//crear una variable que almacene un numero y que por terminal el numero y luego el numero sumado mas uno
+num1=3
+console.log(num1++)
+console.log(num1)
