fd474f7642f5b7e14cad3df1c8c952557994af74
jhonatan-flores <hennryflorescupe@gmail.com>
2024-10-18 12:10:30 -0500

    Feed:introduccion al capitulo tres

---
+++ 3.Estructura_del_Programa/alertas.js
@@ -0,0 +1,13 @@
+// para mostrar un mensaje en mi ventar
+alert("hola")
+
+// para pedir al usuario que elija una opcion
+respuestaConfirm=confirm("escoje")
+
+// para pedir al usuario que ingrese una informacion
+respuestaPrompt=prompt("escribe algo mascota")
+
+console.log(`
+    respuesta de confirm: ${respuestaConfirm},
+    respuesta de prompt: ${respuestaPrompt}
+    `)
